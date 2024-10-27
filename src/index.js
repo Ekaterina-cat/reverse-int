3985c267c052457991b096cccbeaffd711aeab0b
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2024-10-27 12:15:11 +0300

    feat: add solution -reverse-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let num = n.toString().split('');
+    if(num[0] === '-'){
+      num.splice(0, 1)
+    }
+    if(num[num.length - 1] === '0'){
+      num.splice((num.length - 1), 1)
+    }
+  return Number(num.reverse().join(''));
 }
